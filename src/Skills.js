003ade8900865f4f91be0f609ca4ea446be14ddb
jsj487 <jsj487@naver.com>
2024-03-04 14:47:19 +0900

    skill 페이지 완성

--- src/Skills.js
+++ src/Skills.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled, { keyframes } from "styled-components";
 import Header from "./Header";
@@ -25,8 +25,9 @@ const SkillContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   margin: 0 auto;
-  padding: 10rem 2rem;
+  padding: 6rem 2rem;
   position: relative; /* 상위 요소에 상대적 위치 지정 */
   z-index: 1; /* 필요한 경우 z-index 조정 */
   overflow: visible; /* 내부 요소가 바깥으로 나와도 괜찮다면 overflow를 visible로 설정 */
@@ -42,25 +43,28 @@ const SkillContentContainer = styled.div`
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   position: relative; /* 책꽂이에 상대적 위치 지정 */
   z-index: 2; /* z-index를 상위 요소보다 높게 설정 */
+  width: 60%;
 `;
 
 const WhiteBoard = styled.div`
-  width: ${(props) => (props.expanded ? "100%" : "10rem")}; // 조건부 너비 설정
-  height: 15rem;
-  margin: 1rem;
+  width: 10rem; /* 책의 너비를 설정합니다. */
+  height: 15rem; /* 책의 높이를 설정합니다. */
+  margin: 1rem; /* 책과 책 사이의 간격을 설정합니다. */
   padding: 1rem;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column; /* 내용을 세로로 나열합니다. */
+  justify-content: center; /* 내용을 중앙에 배치합니다. */
+  align-items: center; /* 내용을 중앙에 배치합니다. */
   border-radius: 0.5rem;
   background-color: #fff;
   box-shadow: 0.5rem 0.5rem 1rem rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease, transform 0.5s ease, width 0.5s ease; /* width 변화에도 transition 적용 */
+  transition: all 0.3s ease;
   cursor: pointer;
-  overflow: hidden;
-  transform: ${(props) =>
-    props.expanded ? "translateY(370px)" : "translateY(0)"};
+  &:hover {
+    transform: translateY(
+      -10px
+    ); /* 호버 시 책이 살짝 위로 올라가는 효과를 줍니다. */
+  }
 `;
 
 const SkillTitle = styled.div`
@@ -68,6 +72,29 @@ const SkillTitle = styled.div`
   font-weight: 700;
   font-size: 1.5rem;
   color: #f4623a;
+`;
+
+const Monitor = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  background-color: #6a6a6a;
+  margin-top: 20px;
+  padding: 1.5rem 1.5rem;
+  border-radius: 1rem;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 2;
+  width: 50%;
+  height: 40vh;
+`;
+
+const MonitorDisplay = styled.div`
+  background: #1f1f1f;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const RightAnimation = keyframes`
@@ -86,6 +113,7 @@ const NextPage = styled.div`
   top: 50%; // 페이지 하단에 위치하도록 조정합니다.
   font-size: 100px;
   transition: all 2s;
+  z-index: 2;
   svg {
     cursor: pointer;
   }
@@ -100,6 +128,7 @@ const PrevPage = styled.div`
   top: 50%; // 페이지 하단에 위치하도록 조정합니다.
   font-size: 100px;
   transition: all 2s;
+  z-index: 2;
   svg {
     cursor: pointer;
   }
@@ -110,28 +139,58 @@ const PrevPage = styled.div`
 `;
 
 const skillsData = [
-  { title: "FrontEnd" },
-  { title: "VersionControl" },
-  { title: "Certificate" },
-  { title: "Communication" },
-  { title: "Deployment" },
+  { title: "FrontEnd", icon: FrontEndIcon },
+  { title: "VersionControl", icon: VersionControlIcon },
+  { title: "Certificate", icon: CertificateIcon },
+  { title: "Communication", icon: Skills6Icon },
+  { title: "Deployment", icon: Skills7Icon },
 ];
 
 function Skills() {
-  const [expandedBoard, setExpandedBoard] = useState(null);
-  const containerRef = useRef(null); // SkillContentContainer의 참조를 저장
-  const [containerWidth, setContainerWidth] = useState("10rem"); // 초기 너비 값 설정
+  const [expandedBoard, setExpandedBoard] = useState({
+    index: null,
+    expanded: false,
+  });
+  const [displayContent, setDisplayContent] = useState("");
+  const toggleBoard = (index) => {
+    if (expandedBoard.index === index && expandedBoard.expanded) {
+      // 이미 확장된 상태에서 다시 클릭 시, 변화 없이 유지
+      setExpandedBoard({ index, expanded: true });
+    } else {
+      // 새로운 WhiteBoard 클릭 시, 잠깐 초기 상태로 돌아갔다가 새로운 내용으로 변경
+      setDisplayContent("Switching...");
+      setExpandedBoard({ index, expanded: true });
+    }
+  };
 
   useEffect(() => {
-    // SkillContentContainer의 현재 너비를 상태로 설정
-    if (containerRef.current) {
-      setContainerWidth(`${containerRef.current.offsetWidth}px`);
+    if (expandedBoard.expanded && expandedBoard.index !== null) {
+      const timer = setTimeout(() => {
+        setDisplayContent(
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "white",
+            }}
+          >
+            <img
+              src={skillsData[expandedBoard.index].icon}
+              alt={skillsData[expandedBoard.index].title}
+              style={{ maxWidth: "90%", maxHeight: "90%" }}
+            />
+          </div>
+        );
+      }, 500); // 500ms의 딜레이 후 콘텐츠 변경
+      return () => clearTimeout(timer);
+    } else {
+      setDisplayContent("Choose a skill to learn more");
     }
-  }, []); // 의존성 배열이 빈 배열이므로, 컴포넌트 마운트 시에만 실행
+  }, [expandedBoard]);
 
-  const toggleBoard = (index) => {
-    setExpandedBoard(expandedBoard === index ? null : index);
-  };
   const navigate = useNavigate();
   const NextBtn = () => {
     navigate("/projects");
@@ -147,25 +206,26 @@ function Skills() {
         <MdKeyboardDoubleArrowLeft />
       </PrevPage>
       <SkillContainer>
-        <SkillContentContainer ref={containerRef}>
-          {" "}
-          {/* ref 추가 */}
+        <SkillContentContainer>
           {skillsData.map((skill, index) => (
-            <WhiteBoard
-              key={index}
-              expanded={expandedBoard === index}
-              style={{
-                width: expandedBoard === index ? containerWidth : "10rem",
-              }} // style prop으로 너비 조정
-              onClick={() => toggleBoard(index)}
-            >
+            <WhiteBoard key={index} onClick={() => toggleBoard(index)}>
               <SkillTitle>{skill.title}</SkillTitle>
-              {expandedBoard === index && (
-                <div>{/* Additional content here */}</div>
-              )}
             </WhiteBoard>
           ))}
         </SkillContentContainer>
+        <Monitor>
+          <MonitorDisplay>
+            {typeof displayContent === "string" ? (
+              <span
+                style={{ color: "#FFF", fontSize: "30px", fontWeight: "700" }}
+              >
+                {displayContent}
+              </span>
+            ) : (
+              displayContent
+            )}
+          </MonitorDisplay>
+        </Monitor>
       </SkillContainer>
       <NextPage onClick={NextBtn}>
         <MdKeyboardDoubleArrowRight />
